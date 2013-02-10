c7321fb380a1b1cb9091ccef6c1df39d5f3c33c0
Maxii <jim@strategicforge.com>
2013-02-10 09:17:20 -0500

    First gen NGUI GUI, Prefab container GOs rather than Resources.Load, Project and Scene organization changes

---
+++ CodeLibrarySource/CodeEnv.Master.Common.Unity/Game/Framework/GameDate.cs
@@ -0,0 +1,46 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright>
+// Copyright © 2012 - 2013 Strategic Forge
+//
+// Email: jim@strategicforge.com
+// </copyright> 
+// <summary> 
+// File: GameDate.cs
+// COMMENT - one line to give a brief idea of what the file does.
+// </summary> 
+// -------------------------------------------------------------------------------------------------------------------- 
+
+namespace CodeEnv.Master.Common.Unity {
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using CodeEnv.Master.Common;
+    using CodeEnv.Master.Common.LocalResources;
+    using UnityEngine;
+
+    public class GameDate : IGameDate {
+        // IMPROVE StringBuilder?
+        private int daysPerYear = GameValues.DaysPerGameYear;
+        private float gameDaysPerSecond = GameValues.GameDaysPerSecond;
+        private int startingGameYear = GameValues.StartingGameYear;
+
+        public int DayOfYear { get; internal set; }
+
+        public int Year { get; internal set; }
+
+        public string FormattedDate {
+            get {
+                return "{0}.{1:D3}".Inject(Year, DayOfYear);
+            }
+        }
+
+        internal void SyncDateToGameClock(float gameClock) {
+            int elapsedDays = Mathf.FloorToInt(gameClock * gameDaysPerSecond);
+            Year = startingGameYear + Mathf.FloorToInt(elapsedDays / daysPerYear);
+            DayOfYear = 1 + (elapsedDays % daysPerYear);
+        }
+    }
+
+}
+
---
+++ CodeLibrarySource/CodeEnv.Master.ExternalMonoBehaviours/Generic/Debug/DynamicObjects.cs
@@ -0,0 +1,96 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright>
+// Copyright © 2012 - 2013 Strategic Forge
+//
+// Email: jim@strategicforge.com
+// </copyright> 
+// <summary> 
+// File: DynamicObjects.cs
+// Singleton for easy access to DynamicObjects folder in scene.
+// </summary> 
+// -------------------------------------------------------------------------------------------------------------------- 
+
+// default namespace
+
+using System;
+using CodeEnv.Master.Common;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Singleton for easy access to DynamicObjects folder in scene.
+/// </summary>
+public class DynamicObjects : MonoBehaviour {
+
+    #region Custom MonoBehaviour Singleton Pattern
+    private static DynamicObjects instance = null;
+    public static DynamicObjects Instance {
+        get {
+            if (instance == null) {
+                // Instance is required for the first time, so look for it
+                instance = GameObject.FindObjectOfType(typeof(DynamicObjects)) as DynamicObjects;
+                if (instance == null) {
+                    // no instance created yet, so create one
+                    GameObject dynamicObjectsFolder = GameObject.Find(GameValues.DynamicObjectsFolderName);
+                    if (dynamicObjectsFolder != null) {
+                        // if our destination folder exists, add our newly created instance to it
+                        instance = dynamicObjectsFolder.AddComponent<DynamicObjects>();
+                    }
+                    else {
+                        // DynamicObjects folder isn't in the scene, so create it
+                        Debug.LogWarning("No DynamicObjects folder found, so creating one.");
+                        dynamicObjectsFolder = new GameObject(GameValues.DynamicObjectsFolderName, typeof(DynamicObjects));
+                        instance = dynamicObjectsFolder.GetComponent<DynamicObjects>();
+                        if (instance == null) {
+                            Debug.LogError("Problem during the creation of {0}.".Inject(typeof(DynamicObjects).ToString()));
+                        }
+                    }
+                }
+                instance.Initialize();
+            }
+            return instance;
+        }
+    }
+
+    void Awake() {
+        // If no other MonoBehaviour has requested Instance in an Awake() call executing
+        // before this one, then we are it. There is no reason to search for an object as we must be attached to it.
+        if (instance == null) {
+            instance = this as DynamicObjects;
+            instance.Initialize();
+        }
+    }
+
+    // Make sure Instance isn't referenced anymore
+    void OnApplicationQuit() {
+        instance = null;
+    }
+    #endregion
+
+    private void Initialize() {
+        // do any required initialization here as you would normally do in Awake()
+    }
+
+    /// <summary>
+    /// Gets the DynamicObjects folder.
+    /// </summary>
+    /// <value>
+    /// The folder.
+    /// </value>
+    public static Transform Folder {
+        get {
+            if (Instance.gameObject.name != GameValues.DynamicObjectsFolderName) {
+                Debug.LogError("Expecting folder {0} but got {1}.".Inject(GameValues.DynamicObjectsFolderName, Instance.gameObject.name));
+            }
+            return Instance.transform;
+        }
+    }
+
+    public override string ToString() {
+        return new ObjectAnalyzer().ToString(this);
+    }
+
+
+}
+
+
